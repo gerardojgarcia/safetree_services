8382959d262834ef9d04c87588d459a7644de5d4
Gerardo J Garcia <gerardojgarcia@pm.me>
2024-07-16 22:56:23 -0700

    changed hero layout; changed blog layout

--- components/TheHero.vue
+++ components/TheHero.vue
@@ -1,8 +1,8 @@
 <script setup></script>
 <template>
-    <div class="hero grid grid-cols-2 bg-gradient-to-b from-white to-blue-400 from-70% h-screen  text-dark_slate_gray">
+    <div class="hero grid place-items-center pb-32 mt-32 bg-white from-50% text-dark_slate_gray" data-aos="fade-up">
         <div
-            class="heading flex flex-col p-8 items-center justify-center text-dark_slate_gray h-full bg-opacity-85"
+            class="heading flex flex-col p-8 py-44 items-center justify-center text-dark_slate_gray h-full bg-opacity-85"
         >
             <h1 class="text-5xl text-center mb-8 md:px-12 ">
                 Your Trusted Partner in Expert
@@ -21,7 +21,7 @@
                 >
             </div>
         </div>
-        <div class="hero-image grid place-items-center overflow-hidden rounded-xl">
+        <div class="hero-image grid place-items-center w-3/4 pt-18 overflow-hidden rounded-xl" data-aos="fade-up" data-aos-delay="1500">
           <img src="/safeetree_hero.webp" alt="Image of workers doing maintenance service on a tree" />
         </div>
     </div>
--- pages/blog.vue
+++ pages/blog.vue
@@ -8,7 +8,7 @@ useHead({
   <div class="dark:bg-black h-screen dark:text-white">
     <TheHeader />
 
-    <div class="flex h-[65vh] overflow-y-auto">
+    <div class="flex h-[65vh] overflow-y-auto pt-32">
       <AppBlogSideNav class="absolute w-52" />
       <NuxtPage class="ml-52 h-[50vh] " />
     </div>
