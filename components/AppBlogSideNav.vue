455249e9a07c5027ed9368a317830a4bcba7ca56
Gerardo J Garcia <gerardojgarcia@pm.me>
2024-07-19 23:30:44 -0700

    added articles.js to hold article information; updated appblogsidenac

--- components/AppBlogSideNav.vue
+++ components/AppBlogSideNav.vue
@@ -1,55 +1,22 @@
 <script setup>
-const pages = ref([
-    {
-        id: 1,
-        title: "Benefits of Professional Tree Trimming",
-        date: "05-23-2024",
-        description:
-            "Explore the advantages of hiring professionals for tree trimming in residential areas.",
-        link: "/blog/benefits-professional-residential-tree-trimming",
-    },
-    {
-        id: 2,
-        title: "Signs Your Trees Need Trimming",
-        date: "06-19-2024",
-        description:
-            "Learn how to identify when your trees need trimming to maintain their health and safety.",
-        link: "/blog/signs-trees-need-trimming",
-    },
-    {
-        id: 3,
-        title: "Best Time of Year for Tree Trimming in San Bernardino",
-        date: "06-23-2024",
-        description:
-            "Discover the optimal times for tree trimming in San Bernardino County to ensure tree health.",
-        link: "/blog/best-time-tree-trimming-san-bernardino",
-    },
-    {
-        id: 4,
-        title: "DIY Tree Trimming Tips",
-        date: "06-23-2024",
-        description:
-            "Practical tips for safely trimming your trees at home.Practical tips for safely trimming your trees at home.",
-        link: "/blog/diy-tree-trimming-tips",
-    },
-]);
+import articles from "/data/articles.js";
 </script>
 <template>
-  <div>
-    <nav class="side-nav flex flex-col">
-      <NuxtLink
-        :to="page.link"
-        class="hover:text-concrete_gray"
-        v-for="page in pages"
-        :key="page.id"
-        >{{ page.title }}
-      </NuxtLink>
-    </nav>
-  </div>
+    <div>
+        <nav class="side-nav flex flex-col">
+            <NuxtLink
+                :to="articles.link"
+                class="hover:text-concrete_gray"
+                v-for="article in articles"
+                :key="article.id"
+                >{{ article.title }}
+            </NuxtLink>
+        </nav>
+    </div>
 </template>
 <style scoped>
 .router-link-active {
-  font-weight: bold;
-  color: #2563eb;
+    font-weight: bold;
+    color: #2563eb;
 }
 </style>
---
+++ data/articles.js
@@ -0,0 +1,47 @@
+export const articles = ref([
+  {
+    id: 1,
+    title: "Benefits of Professional Tree Trimming",
+    date: "05-23-2024",
+    description:
+      "Explore the advantages of hiring professionals for tree trimming in residential areas.",
+    link: "/blog/benefits-professional-residential-tree-trimming",
+    image: "/safetree_1.webp",
+  },
+  {
+    id: 2,
+    title: "Signs Your Trees Need Trimming",
+    date: "06-19-2024",
+    description:
+      "Learn how to identify when your trees need trimming to maintain their health and safety.",
+    link: "/blog/signs-trees-need-trimming",
+    image: "/safetree_1.webp",
+  },
+  {
+    id: 3,
+    title: "Best Time of Year for Tree Trimming in San Bernardino",
+    date: "06-23-2024",
+    description:
+      "Discover the optimal times for tree trimming in San Bernardino County to ensure tree health.",
+    link: "/blog/best-time-tree-trimming-san-bernardino",
+    image: "/safetree_1.webp",
+  },
+  {
+    id: 4,
+    title: "DIY Tree Trimming Tips",
+    date: "06-23-2024",
+    description:
+      "Practical tips for safely trimming your trees at home.Practical tips for safely trimming your trees at home.",
+    link: "/blog/diy-tree-trimming-tips",
+    image: "/safetree_1.webp",
+  },
+  {
+    id: 5,
+    title: "DIY Tree Trimming Tips",
+    date: "06-23-2024",
+    description:
+      "Practical tips for safely trimming your trees at home.Practical tips for safely trimming your trees at home.",
+    link: "/blog/diy-tree-trimming-tips",
+    image: "/safetree_1.webp",
+  },
+]);
